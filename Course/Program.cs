bd678221ff0103e65bfcc34b2b6d9e9ad6036cb4
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-17 16:28:36 -0300

    Tratamento de erros e moeda br

--- Course/Program.cs
+++ Course/Program.cs
@@ -7,29 +7,40 @@ public class ProcessFile
 {
     public static void Main()
     {
+        CultureInfo idiomaptBR = new CultureInfo("pt-BR");
+
         Console.WriteLine("Enter account data");
         Console.Write("Number: ");
         int number = int.Parse(Console.ReadLine());
+
         Console.Write("Holder: ");
         String holder = Console.ReadLine();
+
         Console.Write("Initial balance: ");
-        double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double balance = double.Parse(Console.ReadLine(), idiomaptBR);
+
         Console.Write("Withdraw limit: ");
-        double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double withdrawLimit = double.Parse(Console.ReadLine(), idiomaptBR);
 
         Account acc = new Account(number, holder, balance, withdrawLimit);
 
         Console.WriteLine();
         Console.Write("Enter amount for withdraw: ");
-        double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double amount = double.Parse(Console.ReadLine(), idiomaptBR);
+
         try
         {
+
             acc.Withdraw(amount);
-            Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("New balance: " + acc.Balance.ToString("C", idiomaptBR));
+
         }
         catch (DomainException e)
         {
+
             Console.WriteLine("Withdraw error: " + e.Message);
+
         }
     }
 }
